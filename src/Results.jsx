b4545b43643bf276a4f4e26df380928b0f089a10
esatsma <lisetteatsma@gmail.com>
2023-06-27 14:28:55 +0200

    component composition

---
+++ src/Results.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No pets found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            animal={pet.animal}
+            name={pet.name}
+            breed={pet.breed}
+            location={`${pet.city}, ${pet.state}`}
+            images={pet.images}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
